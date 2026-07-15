d4a1402f5993d46f8217b3ff425b5c75b454fd87
Jarne <descheerderjarne@gmail.com>
2026-07-15 14:31:58 +0200

    premier test en ligne

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
   root: 'src',
-  base: '/projets/tfe-application/',
+  base: '/projets/tfe-aout/',
   publicDir: '../public',
   build: {
     outDir: '../dist',
