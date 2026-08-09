bbd9df1964e1d5d80a2b3c1d1a85507c9181f40c
Jarne <descheerderjarne@gmail.com>
2026-08-09 14:01:02 +0200

    page les roues

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -470,7 +470,7 @@ fetch("/projets/tfe-aout/api/moi.php")
     }
   });
 
-if (fichier === "moteur.html" || fichier === "freins.html" || fichier === "temoins.html" || fichier === "peinture.html" || fichier === "chassis.html") {
+if (fichier === "moteur.html" || fichier === "freins.html" || fichier === "temoins.html" || fichier === "peinture.html" || fichier === "chassis.html" || fichier === "roues.html" || fichier === "suspension.html") {
   const boutonsMoteur = document.querySelectorAll(
     ".boutonMoteurA, .boutonMoteurB, .boutonMoteurC, .boutonMoteurD, .boutonMoteurE, .boutonMoteurF, .boutonMoteurG"
   );
@@ -489,6 +489,10 @@ if (fichier === "moteur.html" || fichier === "freins.html" || fichier === "temoi
 
   const boutonsChassis = document.querySelectorAll(
     ".boutonChassisA, .boutonChassisB"
+  );
+
+  const boutonsRoues = document.querySelectorAll(
+    ".boutonRouesA, .boutonRouesB, .boutonRouesC"
   );
 
   boutonsMoteur.forEach(btn => {
@@ -516,6 +520,12 @@ if (fichier === "moteur.html" || fichier === "freins.html" || fichier === "temoi
   });
 
   boutonsChassis.forEach(btn => {
+    btn.addEventListener("click", () => {
+      afficherPartie(btn.dataset.value);
+    });
+  });
+
+  boutonsRoues.forEach(btn => {
     btn.addEventListener("click", () => {
       afficherPartie(btn.dataset.value);
     });
