0673b53485c6404ae670795650d2dd72af2e1120
Jarne <descheerderjarne@gmail.com>
2026-08-10 12:18:21 +0200

    connexion

--- public/api/database.php
+++ public/api/database.php
@@ -1,12 +1,12 @@
 <?php
 
-$dsn = "mysql:host=jarnedl561.mysql.db;dbname=jarnedl561;charset=utf8";
-$user = "jarnedl561";
-$pass = "Mignolet2004";
+$dsn  = getenv('DB_DSN')  ?: 'mysql:host=jarnedl561.mysql.db;dbname=jarnedl561;charset=utf8';
+$user = getenv('DB_USER') ?: 'jarnedl561';
+$pass = getenv('DB_PASS') ?: '';
 
 $db = new PDO($dsn, $user, $pass, [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 ]);
 
 ?>
